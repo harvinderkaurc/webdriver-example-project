191e036d2a1253eb456fe18b5093e92bd220602a
joshcooper <josh.cooper@capgemini.com>
2017-09-07 15:20:51 +0100

    test 2

--- src/test/java/dfe/dfeTest.java
+++ src/test/java/dfe/dfeTest.java
@@ -11,7 +11,7 @@ public class dfeTest extends TestBaseQA {
     private boolean acceptNextAlert = true;
     private StringBuffer verificationErrors = new StringBuffer();
 
-//this no longer contains an bla
+//this no longer contains an blaa
     @Test
     public void QAReg() throws Exception {
         driver.get(baseUrl + selfURL);
